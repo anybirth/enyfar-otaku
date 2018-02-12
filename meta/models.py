84f66dbc4ff077c463171751bdf34d8b45c7bf93
tab0902 <mercury.brave.0902@gmail.com>
2018-02-12 02:50:00 +0000

    Add models and migrations in meta app

--- meta/models.py
+++ meta/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    is_supported = models.BooleanField()
+
+    class Meta:
+        verbose_name = "country"
+        verbose_name_plural = 'countries'
+
+    def __str__(self):
+        return '%s' % self.name
+
+class District(models.Model):
+    country = models.ForeignKey('Country', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    suffix = models.CharField(max_length=50, blank=True)
+    is_supported = models.BooleanField()
+
+    class Meta:
+        verbose_name = "district"
+        verbose_name_plural = 'districts'
+
+    def __str__(self):
+        return '%s' % self.name
