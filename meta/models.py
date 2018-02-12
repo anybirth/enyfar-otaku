cc085a89b50b84d701c3326b8bfda9c3a460aeed
tab0902 <mercury.brave.0902@gmail.com>
2018-02-12 05:57:51 +0000

    Add verbose_name to models in meta app

--- meta/models.py
+++ meta/models.py
@@ -1,27 +1,28 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 class Country(models.Model):
-    name = models.CharField(max_length=50)
-    is_supported = models.BooleanField()
+    name = models.CharField(_('name'), max_length=50)
+    is_supported = models.BooleanField(_('supported'), default=False)
 
     class Meta:
-        verbose_name = "country"
-        verbose_name_plural = 'countries'
+        verbose_name = _('country')
+        verbose_name_plural = _('countries')
 
     def __str__(self):
         return '%s' % self.name
 
 class District(models.Model):
-    country = models.ForeignKey('Country', on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    suffix = models.CharField(max_length=50, blank=True)
-    is_supported = models.BooleanField()
+    country = models.ForeignKey('Country', on_delete=models.CASCADE, verbose_name=_('country'))
+    name = models.CharField(_('name'), max_length=50)
+    suffix = models.CharField(_('suffix'), max_length=50, blank=True)
+    is_supported = models.BooleanField(_('supported'), default=False)
 
     class Meta:
-        verbose_name = "district"
-        verbose_name_plural = 'districts'
+        verbose_name = _('district')
+        verbose_name_plural = _('districts')
 
     def __str__(self):
         return '%s' % self.name
