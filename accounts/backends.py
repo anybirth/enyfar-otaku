1df39b58aadfadd6f5f086c4e63c93d4837b0309
tab0902 <mercury.brave.0902@gmail.com>
2018-02-12 06:16:06 +0000

    Add custom auth backends to accounts app

---
+++ accounts/backends.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+
+class EmailOrUsernameModelBackend(ModelBackend):
+
+    def authenticate(self, username=None, password=None):
+        UserModel = get_user_model()
+        if '@' in username:
+            kwargs = {'email': username}
+        else:
+            kwargs = {'username': username}
+        try:
+            user = UserModel.objects.get(**kwargs)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            UserModel().set_password(password)
