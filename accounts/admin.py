1c09085bdd59396cc7c3c1aa4ded1299b4b020b0
tab0902 <mercury.brave.0902@gmail.com>
2018-03-01 13:27:46 +0900

    Add models to admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register([models.User, models.Member, models.MemberAddress, models.Itinerary, models.Transfer, models.Departure, models.Arrival,])
--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register([models.Category, models.Tag, models.Item, models.ItemImage, models.ItemLike,])
