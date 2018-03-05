c13bf5f76120af92de0dc47ec00f9cf3d5c1e6ff
tab0902 <mercury.brave.0902@gmail.com>
2018-03-05 13:29:27 +0900

    Fix a typo

--- anybirth/settings.py
+++ anybirth/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'anybirth.middlewares.MemberMiddleware',
+    # 'anybirth.middlewares.MemberMiddleware',
 ]
 
 ROOT_URLCONF = 'anybirth.urls'
