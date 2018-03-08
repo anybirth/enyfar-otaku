697bb2c2a71992c813929007cb850759d34a30c6
tab0902 <mercury.brave.0902@gmail.com>
2018-03-08 12:23:08 +0900

    Add admin classes of User

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,30 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm,  ReadOnlyPasswordHashField
 from . import models
 
 # Register your models here.
 
-admin.site.register([models.User, models.Member, models.MemberAddress, models.Itinerary, models.Transfer, models.Departure, models.Arrival,])
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = models.User
+        fields = UserCreationForm.Meta.fields + ('email',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = models.User
+        fields = '__all__'
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register([models.Member, models.MemberAddress, models.Itinerary, models.Transfer, models.Departure, models.Arrival,])
