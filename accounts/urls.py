801b01cc6b6b8575e5c7b17269ea9ea077182cc4
tab0902 <mercury.brave.0902@gmail.com>
2018-03-08 12:21:04 +0900

    Fix redirect url after login

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('activate/error', views.ActivateErrorView.as_view(), name='activate_error'),
     path('activate/again', views.ActivateAgainView.as_view(), name='activate_again'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('profile/traveller/<int:pk>', views.ProfileTravellerView.as_view(), name='profile_traveller'),
+    path('profile/traveller/', views.ProfileTravellerView.as_view(), name='profile_traveller'),
 ]
