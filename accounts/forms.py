c98015583324b07ede7394efdc33af01979c10c0
tab0902 <mercury.brave.0902@gmail.com>
2018-03-10 09:00:57 +0900

    Add birthday to form fields

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,20 +1,26 @@
 from django import forms
+from django.utils import timezone
 from . import models
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = models.User
-        fields = ['email', 'username', 'password']
+        fields = ['email', 'username', 'password', 'birthday']
         widgets = {
             'email': forms.EmailInput(),
             'password': forms.PasswordInput(),
+            'birthday': forms.SelectDateWidget(
+                years=range(1900, timezone.now().year),
+                empty_label=("年", "月", "日"),
+            ),
         }
 
     def __init__(self, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
 
-        for fieldname in ['username', 'password']:
+        for fieldname in ['email', 'username', 'password', 'birthday']:
             self.fields[fieldname].help_text = None
+            self.fields[fieldname].required = True
 
 class ActivateForm(forms.Form):
     email = forms.CharField(widget=forms.EmailInput)
