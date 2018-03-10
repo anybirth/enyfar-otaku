fcd264734ef9e4c58ca70b50c494fa7d29d1c6a6
tab0902 <mercury.brave.0902@gmail.com>
2018-03-10 17:54:25 +0900

    Add LineOAuth2

--- anybirth/settings.py
+++ anybirth/settings.py
@@ -118,6 +118,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
     'social_core.backends.twitter.TwitterOAuth',
     'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.line.LineOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
 
