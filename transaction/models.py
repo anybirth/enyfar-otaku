6884a3564e95a52b14f7363d2fdd6cb1c71a57bc
tab0902 <mercury.brave.0902@gmail.com>
2018-03-27 14:25:28 +0900

    Add model 'Proposal'

--- transaction/models.py
+++ transaction/models.py
@@ -3,6 +3,28 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
+class Proposal(models.Model):
+    item = models.ForeignKey('Item', on_delete=models.CASCADE, verbose_name=_('商品ID'))
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, verbose_name=_('ユーザーID'))
+    title = models.CharField(_('提案タイトル'), max_length=50)
+    description = models.TextField(_('提案説明文'), blank=True)
+    delivery_method = models.SmallIntegerField(_('配送方法'))
+    payment_method = models.SmallIntegerField(_('支払方法'))
+    price_proposal = models.IntegerField(_('価格提案'), blank=True)
+    hand_place = models.CharField(_('商品手渡し場所'), max_length=255, blank=True)
+    delivered_at　= models.DateTimeField(_('予定お届け日時'), blank=True, null=True)
+    status = models.SmallIntegerField(_('状態'), default=0)
+    expired_at = models.DateTimeField(_('掲載終了日'), blank=True)
+    created_at = models.DateTimeField(_('作成日時'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('更新日時'), auto_now=True)
+
+    class Meta:
+        verbose_name = _('提案')
+        verbose_name_plural = _('提案')
+
+    def __str__(self):
+        return '%s' % self.item.name
+
 class Request(models.Model):
     item = models.ForeignKey('main.Item', on_delete=models.CASCADE, verbose_name=_('商品ID'))
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, verbose_name=_('ユーザーID'))
