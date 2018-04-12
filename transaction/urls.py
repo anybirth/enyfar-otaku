89d38b14fd77340e5bbe29d9473a962af44aa045
tab0902 <mercury.brave.0902@gmail.com>
2018-04-12 19:44:38 +0900

    Add app name in urls.py

--- transaction/urls.py
+++ transaction/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'transaction'
 urlpatterns = [
     path('order/', views.OrderView.as_view(), name='order'),
     path('order/confirm/<uuid:slug>/', views.OrderConfirmView.as_view(), name='order_confirm'),
