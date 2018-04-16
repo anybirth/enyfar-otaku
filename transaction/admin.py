d125db34ea6f1d864706ab0474ed97ea9b0778b8
tab0902 <mercury.brave.0902@gmail.com>
2018-04-16 16:27:49 +0900

    Remove model 'Proposal'

--- transaction/admin.py
+++ transaction/admin.py
@@ -3,4 +3,4 @@ from . import models
 
 # Register your models here.
 
-admin.site.register([models.Proposal, models.Order,])
+admin.site.register([models.Order,])
--- transaction/models.py
+++ transaction/models.py
@@ -3,29 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
-class Proposal(models.Model):
-    item = models.ForeignKey('main.Item', on_delete=models.CASCADE, verbose_name=_('商品ID'))
-    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, verbose_name=_('ユーザーID'))
-    itinerary = models.ForeignKey('accounts.Itinerary', on_delete=models.CASCADE, verbose_name=_('旅程ID'))
-    title = models.CharField(_('提案タイトル'), max_length=50)
-    description = models.TextField(_('提案説明文'), blank=True)
-    delivery_method = models.SmallIntegerField(_('配送方法'))
-    payment_method = models.SmallIntegerField(_('支払方法'))
-    price_proposal = models.IntegerField(_('価格提案'), blank=True)
-    hand_place = models.CharField(_('商品手渡し場所'), max_length=255, blank=True)
-    delivered_at = models.DateTimeField(_('予定お届け日時'), blank=True, null=True)
-    status = models.SmallIntegerField(_('状態'), default=0)
-    expired_at = models.DateTimeField(_('掲載終了日'), blank=True)
-    created_at = models.DateTimeField(_('作成日時'), auto_now_add=True)
-    updated_at = models.DateTimeField(_('更新日時'), auto_now=True)
-
-    class Meta:
-        verbose_name = _('提案')
-        verbose_name_plural = _('提案')
-
-    def __str__(self):
-        return '%s' % self.user.username
-
 class Order(models.Model):
     item = models.ForeignKey('main.Item', on_delete=models.CASCADE, verbose_name=_('商品ID'))
     requester = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='requester_set', verbose_name=_('リクエスターID'))
