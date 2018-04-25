6ca201a31f47ffe7182d9c5e268b12ab4c794e8a
tab0902 <mercury.brave.0902@gmail.com>
2018-04-25 22:40:43 +0900

    Improve logic of drawer toggle

--- enyfar-otaku/static/js/main.js
+++ enyfar-otaku/static/js/main.js
@@ -1,17 +1,31 @@
 $(function() {
+  var height;
+  var scrollpos;
+  var header = $('#header').height();
+
   $('#dummy').css('height', 99999);
+  $('#drawer').css('padding-top', header);
+  $('#content').css('padding-top', header);
+
   $('#hamburger').on('click', function() {
+    scrollpos = $(window).scrollTop();
+    height = scrollpos - header;
     $('#hamburger').hide();
     $('#cross').fadeIn();
     $('#dummy').fadeIn();
     $('#drawer').animate({height: 'toggle'});
+    $('#content').addClass('fixed').css('top', -scrollpos);
     return false;
   });
+
   $('#cross').on('click', function() {
     $('#cross').hide();
     $('#hamburger').fadeIn();
     $('#dummy').fadeOut();
-    $('#drawer').animate({height: 'toggle'});
+    $('#drawer').animate({height: 'toggle'}, function() {
+      $('#content').removeClass('fixed').css('top', height);
+      $('body,html').animate({scrollTop: scrollpos}, 0);
+    });
     return false;
   });
 });
