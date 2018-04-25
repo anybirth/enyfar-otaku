862182bf06f50a8512ec6fefd998735456d0dcaa
tab0902 <mercury.brave.0902@gmail.com>
2018-04-25 21:26:27 +0900

    Add #dummy

--- enyfar-otaku/static/js/main.js
+++ enyfar-otaku/static/js/main.js
@@ -1,13 +1,16 @@
 $(function() {
+  $('#dummy').css('height', 99999);
   $('#hamburger').on('click', function() {
     $('#hamburger').hide();
     $('#cross').fadeIn();
+    $('#dummy').fadeIn();
     $('#drawer').animate({height: 'toggle'});
     return false;
   });
   $('#cross').on('click', function() {
     $('#cross').hide();
     $('#hamburger').fadeIn();
+    $('#dummy').fadeOut();
     $('#drawer').animate({height: 'toggle'});
     return false;
   });
