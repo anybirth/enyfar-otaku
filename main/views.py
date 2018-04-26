cf2dc9b30251362d2fbc38594f6a7cce6a967434
tab0902 <mercury.brave.0902@gmail.com>
2018-04-26 17:53:54 +0900

    Add section #top-category

--- main/views.py
+++ main/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 from . import models
+from transaction.models import Order
 
 # Create your views here.
 
-class IndexView(generic.ListView):
-    model = models.Item
-    context_object_name = 'items'
+class IndexView(generic.TemplateView):
     template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recommended_categories'] = models.Category.objects.order_by('recommendation_ranking')[:3]
+        return context
