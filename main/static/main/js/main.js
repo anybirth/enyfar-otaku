1eb300580109f7bbc623ddee3e68660ba7f13f31
tab0902 <mercury.brave.0902@gmail.com>
2018-04-26 20:16:22 +0900

    Add section #item-search

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -1,9 +1,44 @@
-$(document).ready(function(){
+$(function() {
+
+  // slider
   $('#top-slide').slick({
     autoplay: true,
     autoplaySpeed: 4000,
     speed: 800,
     arrows: false,
     dots: true,
+  });
+
+  // display toggle
+  var selectStatus = 0;
+  var buttonStatus = 'rec';
+
+  $('[name=select-box]').change(function() {
+    selectStatus = $('[name=select-box]').val();
+    var target = '.select-display-' + selectStatus + '.button-display-' + buttonStatus;
+    $('.display').addClass('display-none');
+    $(target).removeClass('display-none');
+  });
+
+  $('#button-rec').on('click', function() {
+    buttonStatus = 'rec';
+    var target = '.select-display-' + selectStatus + '.button-display-' + buttonStatus;
+    $('#button-new').removeClass('warning');
+    $('#button-new').addClass('secondary');
+    $('#button-rec').removeClass('secondary');
+    $('#button-rec').addClass('warning');
+    $('.display').addClass('display-none');
+    $(target).removeClass('display-none');
+  });
+
+  $('#button-new').on('click', function() {
+    buttonStatus = 'new';
+    var target = '.select-display-' + selectStatus + '.button-display-' + buttonStatus;
+    $('#button-rec').removeClass('warning');
+    $('#button-rec').addClass('secondary');
+    $('#button-new').removeClass('secondary');
+    $('#button-new').addClass('warning');
+    $('.display').addClass('display-none');
+    $(target).removeClass('display-none');
   });
 });
